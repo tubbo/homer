0ea709a78dc89b3787720104f87b3709807b32cd
Tom Scott <tscott@telvue.com>
2014-11-24 22:18:41 -0500

    Move configuration to a sourced file

--- lib/homer.zsh
+++ lib/homer.zsh
@@ -5,7 +5,21 @@
 # Load dependencies
 source "/usr/local/lib/antigen.zsh"
 
-# Load framework files
+# Load the framework
 for file in `ls /usr/local/lib/homer`; do
   source "/usr/local/lib/homer/$file"
 done
+
+# Load user plugins
+source "$HOME/etc/plugins"
+
+# Load user aliases
+source "$HOME/etc/aliases"
+
+# Load user profile configuration
+for file in `ls $HOME/etc/profile.d`; do
+  source "$HOME/etc/profile.d/$file"
+done
+
+# Add user path on top so ~/bin files override the main $PATHs.
+export PATH=$HOME/bin:$PATH
