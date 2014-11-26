8b86e0682dd0ceb18c7d357f0f3eb64c1d89f6a5
Tom Scott <tscott@telvue.com>
2014-11-26 16:05:30 -0500

    Rework some dependent libs

--- lib/homer/paths.zsh
+++ lib/homer/paths.zsh
@@ -1,10 +1,8 @@
 # Documentation PATHs
 MANPATH=/usr/local/opt/coreutils/libexec/gnuman:$MANPATH
 
-export PYTHONPATH=$HOME/.python/lib/python2.7/site-packages/
-export GEMPATH=$HOME/.gem/ruby/2.0.0/bin/
 export JSPATH=$HOME/node_modules/.bin/
-export GEMPATHS=$PWD/bin:$PWD/vendor/gems/bin
+export GEMPATHS=$PWD/bin:$PWD/vendor/bundle/bin
 export PATH=$PYTHONPATH:$GEMPATH:$JSPATH:$GEMPATHS:/bin:$PATH
 
 # Cache the path laid out in this file and `reset_path` if the shell
