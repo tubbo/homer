4a1ae64d6e5852adf1e2b8b97f136900902d2df1
Tom Scott <tscott@telvue.com>
2014-12-31 00:36:02 -0500

    Fix up editing

--- share/homer/editing.zsh
+++ share/homer/editing.zsh
@@ -2,17 +2,23 @@
 #
 # Configuration for text editing.
 
+EDITOR='vi'
+PAGER='more'
+
 # Execute as the configured editor.
 edit() {
   $EDITOR $*
 }
-alias e=editor
+alias e=edit
 
 # Execute as the configured pager.
 page() {
   $PAGER $*
 }
-alias v=reader
+alias v=page
 
 # Print a given string.
 alias p=echo
+
+# Clear the screen
+alias c=clear
