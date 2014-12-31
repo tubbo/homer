d9876f35c83a34fc0d4fcc44ede940c402299669
Tom Scott <tscott@telvue.com>
2014-12-31 02:09:15 -0500

    Don't redefine paths in helperS

--- share/homer/paths.zsh
+++ share/homer/paths.zsh
@@ -1,12 +1,5 @@
 #!/bin/zsh
 
-# Documentation PATHs
-MANPATH=/usr/local/opt/coreutils/libexec/gnuman:$MANPATH
-
-export JSPATH=$HOME/node_modules/.bin/
-export GEMPATH=$PWD/bin:$PWD/vendor/bundle/bin
-export PATH=$JSPATH:$GEMPATH:/bin:$PATH
-
 # Cache the path laid out in this file and `reset_path` if the shell
 # gets confused.
 export DEFAULT_PATH=$PATH
